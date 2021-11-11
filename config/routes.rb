6d98892e4cde72b7f18026a97407f34a18803c2c
Jared Head <jaredhead@jareds-mbp.myfiosgateway.com>
2021-11-11 14:09:33 -0500

    Unstyled home page without navbar or footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root "homes#index"
+
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
     devise_scope :user do
       get 'login', to: "devise/sessions#new"
