95a6c11169228e94a843d2826f40ee4237f82dad
Jared Head <jaredhead@jareds-mbp.myfiosgateway.com>
2021-11-11 21:51:29 -0500

    Added show page functionality, controller tests were already done

--- app/javascript/react/components/UserProjectTile.js
+++ app/javascript/react/components/UserProjectTile.js
@@ -1,34 +1,18 @@
 import React, { useState } from "react"
-import { Redirect } from "react-router-dom"
+import { Redirect, Link } from "react-router-dom"
 
 const UserProjectTile = props => {
   const { project } = props
-  const [editRedirect, setEditRedirect] = useState(false)
-
-  const editFunc = () => {
-    setEditRedirect(true)
-  }
-
-  const deleteFunc = async () => {
-    await fetch(`/api/v1/projects/${project.id}`, {
-      method: "DELETE",
-    })
-    props.reRender()
-  }
-
-  if(editRedirect) {
-    return <Redirect to={`/projects/${project.id}/edit`} />
-  }
 
   return (
     <div>
       <div className="grid-x grid-margin-x">
-          <h2 className="cell small-11">{project.name}</h2>
-        <div className="cell small-1">
-          <button onClick={editFunc} className="button">EDIT</button>
-        </div>
+          <h2 className="cell small-11">
+            <Link to={`/projects/${project.id}`}>
+              {project.name}
+            </Link>
+          </h2>
         <h4 className="cell small-11">{project.description}</h4>
-        <button onClick={deleteFunc} className="button">DELETE</button>
       </div>
       <hr />
     </div>
